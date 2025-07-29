094b815c109d9d04cf45cb147840a443b53b1f03
henriqueacf <henrique.assis145@gmail.com>
2025-07-29 11:30:45 -0300

    update project

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import {SubmitHandler, useForm} from "react-hook-form";
+
+type  Inputs = {
+  name: string;
+  lastName: string;
+};
+
+export default function Home() {
+  const { handleSubmit, register } = useForm<Inputs>()
+  const handleFormSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  }
+
+  return (
+      <div className="container mx-auto">
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+          <input
+              {...register('name')}
+              placeholder="Digite seu nome"
+              className="border border-white p-3 text-black"
+          />
+
+          <input
+              {...register('lastName')}
+              placeholder="Digite seu sobrenome"
+              className="block mt-4 border border-white p-3 text-black"
+          />
+
+          <input type="submit" value="Enviar" />
+        </form>
+      </div>
+  );
+}
